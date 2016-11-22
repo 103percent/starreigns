bf4d9a24efd32f99882f2a05e08ebe5ee356f650
Jak Marshalll <JMarshall@sega.co.uk>
2016-11-22 10:45:11 +0000

    Introduced data field to cards that allow further cards to be immediately triggered by the player's choices. Also tried to conform to coding conventions

--- deckSystemDesign/abstractFramework.py
+++ deckSystemDesign/abstractFramework.py
@@ -1,28 +1,28 @@
 ## Variables ##
  
-allCards          #(array of objects): Collection of JSON documents (for example, describing each card).
-eligibleCards   #(array of objects): A subset of the allCards collection. Cards that could be shown to player and their weightings.
+all_cards          #(array of objects): Collection of JSON documents (for example, describing each card).
+eligible_cards   #(array of objects): A subset of the allCards collection. Cards that could be shown to player and their weightings.
 encounters     #(int): The number of encounters the player will see before having to wait. 
-playerState     # (array of objects): Describes the current state of the core values, the narrative(s) underway, and a history of relevant choices made by the player. 
+player_state     # (array of objects): Describes the current state of the core values, the narrative(s) underway, and a history of relevant choices made by the player. 
 
 ## Example of Card ##
 
 { 
     "unique_id" : "ENGINEER-UNDERATTACK-002" , 
     "encounter_string" : "The hull is severely compromised by enemy fire, should we direct additional power to the shields?", 
-    "card_image" : "pictureofengineer.jpeg",
+    "card_image" : "picture_of_engineer.jpeg",
 	"set": "basic_engineer" # cards can only belong to one set.
-	"tags": {["engineer", "underattack"]}  
+	"tags": {["engineer", "under_attack"]}  
 	"prerequisites":{  #Things which MUST be true in order for the card to be considered eligible.
-		"playerState.core.hull": {"$lte": 25},
-		"playerState.setsOwned": {"$includes": "basic_engineer"}
+		"player_state.core.hull": {"$lte": 25},
+		"player_state.setsOwned": {"$includes": "basic_engineer"}
 	},
 	"weights":{  #Things which influence the probability of this card showing up or not if it is eligible.
-		"baseWeight": 100,
-		"playerState.history": {
+		"base_weight": 100,
+		"player_state.history": {
 		"range": {[0,9]} # Last 10 cards swiped
 		"tags": {"$includes": "engineer" } # for each one that has the engineer tag
-		"adjustWeight" : -60 # lower weight by 60 to a minimum of zero
+		"adjust_weight" : -60 # lower weight by 60 to a minimum of zero
 		},
 	},
 		
@@ -30,14 +30,15 @@ playerState     # (array of objects): Describes the current state of the core va
     "leftdata" : { # What the 'left' choice is and what happens if you choose it.
         "choice_string" : "Yes",
 		"consequences": {
-			"playerState.core.power": -20
-		}	
+			"player_state.core.power": -20
+		},
+		"trigger_event": "ALIENS-RETREATING-SPECIAL" # Reference to a card to show next. Does not decrease the encounters total. May be the only way to see this card at all. 	
 	},
 	  "rightdata" : { # What the 'right' choice is and what happens if you choose it.
         "choice_string" : "No",
 		"consequences": {
-			"playerState.core.hull": -30,
-			"playerState.core.crew": -15
+			"player_state.core.hull": -30,
+			"player_state.core.crew": -15
 		}	
 	},	 
 }
@@ -45,10 +46,10 @@ playerState     # (array of objects): Describes the current state of the core va
 ## Session Behaviour ##
 
 while(encounters > 0){
-from eligibleCards #sample a card based on their weights, making any adjustments that the cards demand;
+from eligible_cards #sample a card based on their weights, making any adjustments that the cards demand;
 Resolve the effects of the player's decision;
 encounters -= 1;
-update eligibleCards;
+update eligible_cards;
 }
 Start timer to give the player more encounters;
 Tell the player to go play with their balls until the encounters regenerate;
